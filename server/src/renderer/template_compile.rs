d8c51b5cb582f3e9d10a1b0a12b8c37c48c782b4
ren-wei <demonre@foxmail.com>
2025-03-27 14:11:44 +0800

    fix: 出现 v-if 的标签上的表达式被忽略

--- server/src/renderer/template_compile.rs
+++ server/src/renderer/template_compile.rs
@@ -92,11 +92,6 @@ fn compile_node(node: &Node, source: &str, result: &mut TemplateCompileResult) {
             if let Some(value) = &value.value {
                 if value.starts_with(r#"""#) && value.ends_with(r#"""#) && value.len() > 1 {
                     let value = &value[1..value.len() - 1];
-                    if key == v_if_key {
-                        skip_util_v_if = false;
-                    } else if key == v_else_if_key {
-                        skip_util_v_else_if = false;
-                    }
                     if key == v_for_key {
                         if let Some((left, right)) = value.split_once(" in ") {
                             if let Some(caps) = REG_V_FOR_WITH_INDEX.captures(left) {
@@ -139,6 +134,10 @@ fn compile_node(node: &Node, source: &str, result: &mut TemplateCompileResult) {
                     }
                 }
             }
+        } else if skip_util_v_if && key == v_if_key {
+            skip_util_v_if = false;
+        } else if skip_util_v_else_if && key == v_else_if_key {
+            skip_util_v_else_if = false;
         }
     }
 
@@ -275,6 +274,11 @@ mod tests {
             r#"<template><ProjectHeader v-if="showHeader" title="header" /><Empty v-else-if="showEmpty" /></template>"#,
             "if(showHeader){}else if(showEmpty){}",
             &[(3, 31, 10), (24, 78, 9)],
+        );
+        assert_render(
+            r#"<template><ProjectHeader v-if="showHeader" :title="title" /><Empty /></template>"#,
+            "if(showHeader){(title);}",
+            &[(3, 31, 10), (16, 51, 5)],
         );
         // 位于 v-if 之前的表达式暂时跳过
         assert_render(
